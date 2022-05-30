f8e8e4d345bb8bc8e524df56d54c68e11839bf9d
Lucas <65928307+aquelemesmo@users.noreply.github.com>
2022-05-30 11:25:18 -0300

    Add files via upload

---
+++ javascript/v13/src/comandos/economia/work.quickdb.js
@@ -0,0 +1,11 @@
+//Command work
+//Exemplo: !work
+//Linguagem usada: js
+//Author: aquelemesmoojack#4306
+
+const Discord = require("discord.js")
+const db = require("quick.db")
+
+module.exports.run = async (client, message, args) => {
+    let timeout = 300000 //5 minutos
+}
