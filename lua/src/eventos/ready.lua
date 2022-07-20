5747d37f42846202db1a9bb78db87d02a524f4a3
Lucas <65928307+aquelemesmo@users.noreply.github.com>
2022-07-20 13:42:01 -0300

    Update 1.2.6

---
+++ lua/src/eventos/ready.lua
@@ -0,0 +1,7 @@
+--Ready event
+--Linguagem usada: lua
+--Author: aquelemesmoojack#4306
+
+client.on('ready', function()
+    print('Bot ligado com sucesso! ligado como'.. client.user.username)
+end)
