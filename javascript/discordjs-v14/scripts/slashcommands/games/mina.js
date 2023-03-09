7d9d3f5675a8eb6b0c4edeb2fe95298fd052b2cb
Manji118 <110692699+Manji118@users.noreply.github.com>
2023-03-09 18:34:34 -0300

    mina.js
    Comando de mina de bombas

---
+++ javascript/discordjs-v14/scripts/slashcommands/games/mina.js
@@ -0,0 +1,29 @@
+const { Minesweeper } = require('discord-gamecord'); 
+  module.exports = {
+    name: "mina",
+    description: "Jogue o jogo mina de bombas!",
+    run: async(client, interaction, args ) => {
+ const Game = new Minesweeper({ 
+   message: interaction, 
+   isSlashGame: true, 
+   embed: { 
+     title: 'Mina de bombas', 
+     color: '#5865F2', 
+     description: 'Clique em algum botão para jogar.' 
+   }, 
+   emojis: { flag: '🚩', mine: '💣' }, 
+   mines: 5, 
+   timeoutTime: 60000, 
+   winMessage: 'Parabéns! Você Ganhou, conseguiu cavar todas as partes sem explodir.', 
+   loseMessage: 'Você Perdeu, após você cavando, acabou caindo na bomba.', 
+   playerOnlyMessage: 'Apenas {player} pode usar estes botões' 
+ }); 
+  
+ Game.startGame(); 
+ Game.on('gameOver', result => { 
+   console.log(result);  // =>  { result... } 
+ });
+    }
+  }
+
+///// créditos: Manji118
